7dfd75cb65318b8847f906ff90646089edf10561
deottoni <deottoni@gmail.com>
2019-05-13 23:10:57 -0500

    reorganized folders

---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+// @TODO: YOUR CODE HERE!
+const svgWidth = 960;
+const svgHeight = 500;
+
+const margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+const width = svgWidth - margin.left - margin.right;
+const height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+const svg = d3
+  .select(".scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+const chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  // Initial Params
+const chosenXAxis = "../data/data.csv";
+
+// function used for updating x-scale const upon click on axis label
+function xScale(hairData, chosenXAxis) {
+  // create scales
+  const xLinearScale = d3.scaleLinear()
+    .domain([d3.min(hairData, d => d[chosenXAxis]) * 0.8,
+      d3.max(hairData, d => d[chosenXAxis]) * 1.2
+    ])
+    .range([0, width]);
+
+  return xLinearScale;
+
+}
